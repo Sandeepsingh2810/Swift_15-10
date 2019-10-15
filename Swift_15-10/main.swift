bd28ceea4aa0a814e870d4b2e47922329214995e
sandeepsingh2810 <sandeepjangra0100@gmial.com>
2019-10-15 18:15:21 -0400

    swift 1510

---
+++ Swift_15-10/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift_15-10
+//
+//  Created by MacStudent on 2019-10-15.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
